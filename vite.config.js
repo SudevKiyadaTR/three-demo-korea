e53ac3571c26fb8507f6fd164f8f508aa2f409f7
Sudev Kiyada <sudev.kiyada@thomsonreuters.com>
2022-09-28 09:22:36 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+const ASSET_URL = process.env.ASSET_URL || '';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/three-demo-korea/',
-})
+export default { 
+  base: `${ASSET_URL}/dist/`
+}
